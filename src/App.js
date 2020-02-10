b391ef5296be3e88b365d3b92c4166ff7c38ea02
Tzikas <niko.tzikas@gmail.com>
2020-02-10 17:56:21 -0500

    iteration2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import './App.css';
-import Welcome from './Welcome' //component stateful class
-import Goodbye from './Goodbye' //component stateless functional 
-//stateless or functional components 
-
+import NavBar from './Navbar'
+import FormField from './FormField'
 function App() {
   return (
     <div className="App">
-      <Welcome 
-        emoji=":)" 
-      />
-      <Goodbye 
-        emoji=":(" 
-        />
+        <NavBar />
+        <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+        <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
+        <FormField label="Telephone" type="number" placeholder="555-555-5555" />
+        <FormField label="CreditCard" type="number" placeholder="1234567890" />
+
     </div>
   );
 }
